5f31d8f0d9191e6ccfbf1b407972d0a5ae9991d1
Kraktun <30228269+Kraktun@users.noreply.github.com>
2019-03-11 14:43:23 +0100

    Add filter command

---
+++ src/main/kotlin/com/miche/krak/kBot/commands/FilterCommand.kt
@@ -0,0 +1,28 @@
+package com.miche.krak.kBot.commands
+
+import com.miche.krak.kBot.utils.Privacy
+import com.miche.krak.kBot.utils.Target
+import org.telegram.telegrambots.meta.api.objects.Chat
+import org.telegram.telegrambots.meta.api.objects.User
+
+class FilterCommand(val targets : List<Target> = listOf(Target.USER, Target.GROUP, Target.CHANNEL),
+                    val privacy : Privacy = Privacy.USER,
+                    val argsNum : Int = 0) {
+
+    fun filterFrom(user : User, chat : Chat) : Boolean {
+        val userStatus = Privacy.ADMIN //TODO CHECK FROM DB
+        if (userStatus.ordinal < privacy.ordinal)
+            return false
+        val chatValue = if (chat.isGroupChat || chat.isSuperGroupChat) { Target.GROUP }
+            else if (chat.isChannelChat) {Target.CHANNEL } else {Target.USER}
+        if (!targets.contains(chatValue))
+            return false
+        return true
+    }
+
+    fun filterFormat(arguments : Array<String>) : Boolean {
+        return arguments.size >= argsNum
+    }
+
+
+}
---
+++ src/main/kotlin/com/miche/krak/kBot/utils/Enums.kt
@@ -0,0 +1,9 @@
+package com.miche.krak.kBot.utils
+
+enum class Target {
+    USER, GROUP, CHANNEL
+}
+
+enum class Privacy {
+    USER, POWER_USER, ADMIN, DEV
+}

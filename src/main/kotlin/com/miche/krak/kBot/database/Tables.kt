821d3a3ceac366b1f7fb79b882848dfc322fe2d0
Kraktun <30228269+Kraktun@users.noreply.github.com>
2019-03-14 17:14:26 +0100

    Init DB

---
+++ src/main/kotlin/com/miche/krak/kBot/database/Tables.kt
@@ -0,0 +1,6 @@
+package com.miche.krak.kBot.database
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+// https://www.kotlinresources.com/library/exposed/

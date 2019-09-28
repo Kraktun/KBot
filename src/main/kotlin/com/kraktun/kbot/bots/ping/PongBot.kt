521f1de19d2bb2e9a736e34ca4d1dbe9fecabe23
Kraktun <kraktun.dev@gmail.com>
2019-09-28 18:19:31 +0200

    Added router ip command to pongbot

--- src/main/kotlin/com/kraktun/kbot/bots/ping/PongBot.kt
+++ src/main/kotlin/com/kraktun/kbot/bots/ping/PongBot.kt
@@ -1,6 +1,8 @@
 package com.kraktun.kbot.bots.ping
 
 import com.kraktun.kbot.*
+import com.kraktun.kbot.commands.core.CommandProcessor
+import com.kraktun.kbot.commands.private.RouterIpCommand
 import com.kraktun.kbot.utils.simpleMessage
 import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
@@ -25,6 +27,10 @@ class PongBot(options: DefaultBotOptions) : TelegramLongPollingBot(options) {
         return PING_RECEIVER_TOKEN
     }
 
+    init {
+        CommandProcessor.registerCommand(botUsername!!, RouterIpCommand().engine)
+    }
+
     /**
      * On update: check for pings and answer pong
      */
@@ -32,5 +38,7 @@ class PongBot(options: DefaultBotOptions) : TelegramLongPollingBot(options) {
         val message = if (update.channelPost != null) update.channelPost else update.message
         if (message.hasText() && message.chat.id == PING_PONG_CHAT && message.text == PING)
             simpleMessage(this, PONG, message.chat)
+        else
+            CommandProcessor.fireCommand(message, this)
     }
 }
